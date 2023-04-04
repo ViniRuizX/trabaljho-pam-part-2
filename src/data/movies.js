c489f45fc9412314d6ceeec92a7b417cd91afced
unknown <you@example.com>
2023-04-04 12:20:25 -0300

    Lista de Filmes

---
+++ src/data/movies.js
@@ -0,0 +1,64 @@
+const DATA = 
+[
+    {
+        id: '01',
+        nome: 'Adão Negro',
+        nota: '8,8',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '02',
+        nome: 'Avatar Caminho da Água',
+        nota: '8,8',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '03',
+        nome: 'Telefone Preto',
+        nota: '7,5',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '04',
+        nome: 'The Batman',
+        nota: '10,0',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '05',
+        nome: 'Gato de Botas 2',
+        nota: '7,0',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '06',
+        nome: 'Tudo Normal no Front',
+        nota: '9,5',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '07',
+        nome: 'Interestelar',
+        nota: '9,5',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '08',
+        nome: 'Carros 3',
+        nota: '7,5',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '09',
+        nome: 'Invencível',
+        nota: '9,0',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '10',
+        nome: 'O Homem Aranha: Sem Volta pra Casa',
+        nota: '8,0',
+        imagem: 'adao.jpg'
+    },
+    
+]
