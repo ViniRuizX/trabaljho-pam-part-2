c4f02596bd087396b74d0f8c395b5f5818718284
Vinicius Ruiz <99328998+ViniRuizX@users.noreply.github.com>
2023-04-11 11:04:16 -0300

    Add files via upload

--- src/data/movies.js
+++ src/data/movies.js
@@ -10,55 +10,55 @@ const DATA =
         id: '02',
         nome: 'Avatar Caminho da Água',
         nota: '8,8',
-        imagem: 'adao.jpg'
+        imagem: 'avatar.jpg'
     },
     {
         id: '03',
         nome: 'Telefone Preto',
         nota: '7,5',
-        imagem: 'adao.jpg'
+        imagem: 'telefonepreto.jpg'
     },
     {
         id: '04',
         nome: 'The Batman',
         nota: '10,0',
-        imagem: 'adao.jpg'
+        imagem: 'batman.jpg'
     },
     {
         id: '05',
         nome: 'Gato de Botas 2',
         nota: '7,0',
-        imagem: 'adao.jpg'
+        imagem: 'gatodebotas.jpg'
     },
     {
         id: '06',
         nome: 'Tudo Normal no Front',
         nota: '9,5',
-        imagem: 'adao.jpg'
+        imagem: 'nofront.jpg'
     },
     {
         id: '07',
         nome: 'Interestelar',
         nota: '9,5',
-        imagem: 'adao.jpg'
+        imagem: 'interrestelar.jpg'
     },
     {
         id: '08',
         nome: 'Carros 3',
         nota: '7,5',
-        imagem: 'adao.jpg'
+        imagem: 'carros3.jpg'
     },
     {
         id: '09',
         nome: 'Invencível',
         nota: '9,0',
-        imagem: 'adao.jpg'
+        imagem: 'invencivel.jpg'
     },
     {
         id: '10',
         nome: 'O Homem Aranha: Sem Volta pra Casa',
         nota: '8,0',
-        imagem: 'adao.jpg'
+        imagem: 'homemaranha.jpg'
     },
     
 ]
